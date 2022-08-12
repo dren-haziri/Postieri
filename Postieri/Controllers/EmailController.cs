268cb033ae1445a482e34ac1d56f3c88d5b7e1c1
Ibrahimejupi <100682626+Ibrahimejupi@users.noreply.github.com>
2022-08-12 11:33:55 +0200

    P7-Emailing-Added EmailController

---
+++ Postieri/Controllers/EmailController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Postieri.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly ISendGridClient _sendGridClient;
+        private readonly IConfiguration _configuration;
+
+        public EmailController(ISendGridClient sendGridClient, IConfiguration configuration)
+        {
+            _sendGridClient = sendGridClient;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("send-text-mail")]
+        public async Task<IActionResult> SendPlainTextEmail(string toEmail, string subject, string body)
+        {
+            string fromEmail = _configuration.GetSection("SendGrindEmailSettings").GetValue<string>("FromEmail");
+
+            string fromName = _configuration.GetSection("SendGrindEmailSettings").GetValue<string>("FromName");
+
+            var msg = new SendGridMessage
+            {
+                From = new EmailAddress(fromEmail, fromName),
+                Subject = subject,
+                PlainTextContent = body
+            };
+
+            msg.AddTo(toEmail);
+
+            var response = await _sendGridClient.SendEmailAsync(msg);
+
+            string message = response.IsSuccessStatusCode ? "Email Send" : "Email Sendin Failed";
+            return Ok(message);
+        }
+    }
+}
