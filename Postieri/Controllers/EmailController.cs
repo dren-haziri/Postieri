5b05b655ceaa8b3c8558314c67cc25cc17db2645
Ibrahimejupi <100682626+Ibrahimejupi@users.noreply.github.com>
2022-08-15 14:59:50 +0200

    P7-Email-Changes

---
+++ Postieri/Controllers/EmailController.cs
@@ -0,0 +1,30 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+using Postieri.DTOs;
+using Postieri.Interfaces;
+
+namespace Postieri.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            _emailService.SendEmail(request);
+
+            return Ok();
+    }  }
+}
---
+++ Postieri/DTOs/EmailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Postieri.DTOs
+{
+    public class EmailDto
+    {
+        public string From { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
