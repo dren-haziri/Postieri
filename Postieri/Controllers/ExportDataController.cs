9bcf7a80aca96a99b7809af1e4e819cd44060726
Ibrahimejupi <100682626+Ibrahimejupi@users.noreply.github.com>
2022-08-22 08:59:54 +0200

    P9_Data_Export Added Controller and Model

---
+++ Postieri/Controllers/ExportDataController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using System.Data;
+using System.IO;
+using Postieri.Models;
+
+namespace Postieri.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExportDataController : ControllerBase
+    {
+
+        ExportData dataObj = new ExportData();
+
+        [HttpGet]
+        public IActionResult exportData()
+        {
+            var document = new Document
+            {
+                PageInfo = new PageInfo { Margin = new MarginInfo(28, 28, 28, 40) }
+            };
+            var pdfpage = document.Pages.Add();
+            Table table = new Table()
+            {
+                ColumnWidths = "8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3%",
+                DefaultCellPadding= new MarginInfo(10, 5, 10, 5),
+                Border = new BorderInfo(BorderSide.All,.5f,Color.Black),
+                DefaultCellBorder = new BorderInfo(BorderSide.All,.2f,Color.Black)
+                
+            };
+            DataTable dataTable = dataObj.GetRecord();
+            table.ImportDataTable(dataTable, true, 0, 0);
+            document.Pages[1].Paragraphs.Add(table);
+            using (var streamout = new MemoryStream())
+            {
+                document.Save(streamout);
+                return new FileContentResult(streamout.ToArray(), "application/pdf")
+                {
+                    FileDownloadName = "DataPerformance"
+                };
+            }
+        }
+    }
+}
---
+++ Postieri/Models/ExportData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Postieri.Models
+{
+    public class ExportData
+    {
+        SqlConnection connectDb = new SqlConnection("Data Source=sql.bsite.net\\MSSQL2016;Initial Catalog=postieri_SampleDB;User ID=postieri_SampleDB;Password=DBSamplePW;Persist Security Info=True;");
+        public DataTable GetRecord()
+        {
+            SqlCommand dbCommand = new SqlCommand("SELECT * FROM Orders", connectDb);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(dbCommand);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+    }
+}
