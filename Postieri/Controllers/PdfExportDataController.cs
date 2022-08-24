4a155f823bcba6b127fdce0db7b1484ab3b4ab9d
Ibrahimejupi <100682626+Ibrahimejupi@users.noreply.github.com>
2022-08-24 18:12:00 +0200

    P9_Export_Data used list instead of datatable

--- Postieri/Controllers/PdfExportDataController.cs
+++ Postieri/Controllers/PdfExportDataController.cs
@@ -9,8 +9,9 @@ using System.Data;
 using Postieri.Data;
 using Postieri.Models;
 using System.Collections.Generic;
-using Aspose.Pdf;
 using System.ComponentModel;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
 
 namespace Postieri.Controllers
 {
@@ -19,33 +20,12 @@ namespace Postieri.Controllers
     public class PdfExportDataController : ControllerBase
     {
         private readonly DataContext dbContext;
-        private string dataDir;
 
         public PdfExportDataController(DataContext dbContext)
         {
             this.dbContext = dbContext;
         }
-        public static DataTable ToDataTable<T>(IList<T> data)
-        {
-            PropertyDescriptorCollection props =
-                TypeDescriptor.GetProperties(typeof(T));
-            DataTable table = new DataTable();
-            for (int i = 0; i < props.Count; i++)
-            {
-                PropertyDescriptor prop = props[i];
-                table.Columns.Add(prop.Name, prop.PropertyType);
-            }
-            object[] values = new object[props.Count];
-            foreach (T item in data)
-            {
-                for (int i = 0; i < values.Length; i++)
-                {
-                    values[i] = props[i].GetValue(item);
-                }
-                table.Rows.Add(values);
-            }
-            return table;
-        }
+        
 
         [HttpGet]
         public IActionResult exporPdfData()
@@ -63,24 +43,86 @@ namespace Postieri.Controllers
 
             double totalPrice = performance.Sum(item => item.Price);
 
-            DataTable dataTable = ToDataTable(performance);
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
-            var document = new Document
+            PdfDocument document = new PdfDocument();
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            gfx.DrawString("Data Performance", new XFont("Arial", 30, XFontStyle.Bold), XBrushes.Red, new XPoint(200, 70));
+            gfx.DrawLine(new XPen(XColor.FromArgb(50, 30, 200)), new XPoint(100, 100), new XPoint(500, 100));
+
+            gfx.DrawString("Order Id", new XFont("Arial", 15, XFontStyle.Bold), XBrushes.Black, new XPoint(50, 180));
+            gfx.DrawString("Product Id", new XFont("Arial", 15, XFontStyle.Bold), XBrushes.Black, new XPoint(220, 180));
+            gfx.DrawString("Date", new XFont("Arial", 15, XFontStyle.Bold), XBrushes.Black, new XPoint(400, 180));
+            gfx.DrawString("Price", new XFont("Arial", 15, XFontStyle.Bold), XBrushes.Black, new XPoint(500, 180));
+
+            gfx.DrawLine(new XPen(XColor.FromArgb(50, 30, 200)), new XPoint(50, 190), new XPoint(550, 190));
+
+            int currentYposition_values = 203;
+            int currentYposition_lines = 210;
+
+            if (performance.Count <= 20)
             {
-                PageInfo = new PageInfo { Margin = new MarginInfo(28, 28, 28, 40) }
-            };
-            var pdfpage = document.Pages.Add();
-            Table table = new Table()
+                for (int i = 0; i < performance.Count; i++)
+                {
+                    gfx.DrawString(performance[i].OrderId.ToString(), new XFont("Arial", 9, XFontStyle.Bold), XBrushes.Black, new XPoint(50, currentYposition_values));
+                    gfx.DrawString(performance[i].ProductId.ToString(), new XFont("Arial", 9, XFontStyle.Bold), XBrushes.Black, new XPoint(220, currentYposition_values));
+                    gfx.DrawString(performance[i].Date.ToString(), new XFont("Arial", 9, XFontStyle.Bold), XBrushes.Black, new XPoint(400, currentYposition_values));
+                    gfx.DrawString(performance[i].Price.ToString(), new XFont("Arial", 9, XFontStyle.Bold), XBrushes.Black, new XPoint(500, currentYposition_values));
+
+                    gfx.DrawLine(new XPen(XColor.FromArgb(50, 30, 200)), new XPoint(50, currentYposition_lines), new XPoint(550, currentYposition_lines));
+
+
+                    currentYposition_lines += 20;
+                    currentYposition_lines += 20;
+                }
+                gfx.DrawString(totalPrice.ToString(), new XFont("Arial", 9, XFontStyle.Bold), XBrushes.Red, new XPoint(500, 780));
+            }
+            else
             {
-                ColumnWidths = "20% 20% 20% 20% 20%",
-                DefaultCellPadding = new MarginInfo(10, 5, 10, 5),
-                Border = new BorderInfo(BorderSide.All, .5f, Color.Black),
-                DefaultCellBorder = new BorderInfo(BorderSide.All, .2f, Color.Black)
+                for (int i = 0; i < 15; i++)
+                {
+                    gfx.DrawString(performance[i].OrderId.ToString(), new XFont("Arial", 15, XFontStyle.Bold), XBrushes.Black, new XPoint(100, 280));
+                    gfx.DrawString(performance[i].ProductId.ToString(), new XFont("Arial", 15, XFontStyle.Bold), XBrushes.Black, new XPoint(100, 280));
+                    gfx.DrawString(performance[i].Date.ToString(), new XFont("Arial", 15, XFontStyle.Bold), XBrushes.Black, new XPoint(100, 280));
+                    gfx.DrawString(performance[i].Price.ToString(), new XFont("Arial", 15, XFontStyle.Bold), XBrushes.Black, new XPoint(100, 280));
 
-            };
 
-            table.ImportDataTable(dataTable, true, 0, 0);
-            document.Pages[1].Paragraphs.Add(table);
+                    currentYposition_lines += 20;
+                    currentYposition_lines += 20;
+
+                    performance.Remove(performance[i]);
+                }
+                page = document.AddPage();
+                gfx = XGraphics.FromPdfPage(page);
+                currentYposition_values = 33;
+                currentYposition_lines = 40;
+
+                bool firstpage = true;
+                for(int i = 0; i < performance.Count; i++)
+                {
+                    if(i != 0 && i % 30 == 0)
+                    {
+                        page = document.AddPage();
+                        gfx = XGraphics.FromPdfPage(page);
+                        currentYposition_values = 33;
+                        currentYposition_lines = 40;
+                    }
+                    gfx.DrawString(performance[i].OrderId.ToString(), new XFont("Arial", 9, XFontStyle.Bold), XBrushes.Black, new XPoint(50, currentYposition_values));
+                    gfx.DrawString(performance[i].ProductId.ToString(), new XFont("Arial", 9, XFontStyle.Bold), XBrushes.Black, new XPoint(220, currentYposition_values));
+                    gfx.DrawString(performance[i].Date.ToString(), new XFont("Arial", 9, XFontStyle.Bold), XBrushes.Black, new XPoint(400, currentYposition_values));
+                    gfx.DrawString(performance[i].Price.ToString(), new XFont("Arial", 9, XFontStyle.Bold), XBrushes.Black, new XPoint(500, currentYposition_values));
+
+                    gfx.DrawLine(new XPen(XColor.FromArgb(50, 30, 200)), new XPoint(50, currentYposition_lines), new XPoint(550, currentYposition_lines));
+
+
+                    currentYposition_lines += 20;
+                    currentYposition_lines += 20;
+                }
+                gfx.DrawString(totalPrice.ToString(), new XFont("Arial", 9, XFontStyle.Bold), XBrushes.Red, new XPoint(500, 780));
+            }
+
             using (var streamout = new MemoryStream())
             {
                 document.Save(streamout);
