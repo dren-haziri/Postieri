65e7f18e45cbde8f70443741d5a2d81fbcc588f3
Ibrahimejupi <100682626+Ibrahimejupi@users.noreply.github.com>
2022-08-24 08:59:44 +0200

    P9_Data_Export Added queries

---
+++ Postieri/Controllers/PdfExportDataController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Data;
+using Postieri.Data;
+using Postieri.Models;
+using System.Collections.Generic;
+using Aspose.Pdf;
+using System.ComponentModel;
+
+namespace Postieri.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfExportDataController : ControllerBase
+    {
+        private readonly DataContext dbContext;
+
+        public PdfExportDataController(DataContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public static DataTable ToDataTable<T>(IList<T> data)
+        {
+            PropertyDescriptorCollection props =
+                TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            for (int i = 0; i < props.Count; i++)
+            {
+                PropertyDescriptor prop = props[i];
+                table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+            object[] values = new object[props.Count];
+            foreach (T item in data)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item);
+                }
+                table.Rows.Add(values);
+            }
+            return table;
+        }
+
+        [HttpGet]
+        public IActionResult exporPdfData()
+        {
+            DateTime dateTimeToday = DateTime.Now;
+
+            var dataSet = (from a in dbContext.Orders
+                           where a.Date.Month > dateTimeToday.Month
+                           orderby a.Date ascending
+                           select a).ToList();
+
+            DataTable dataTable = ToDataTable(dataSet);
+
+            var document = new Document
+            {
+                PageInfo = new PageInfo { Margin = new MarginInfo(28, 28, 28, 40) }
+            };
+            var pdfpage = document.Pages.Add();
+            Table table = new Table()
+            {
+                ColumnWidths = "8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3% 8.3%",
+                DefaultCellPadding = new MarginInfo(10, 5, 10, 5),
+                Border = new BorderInfo(BorderSide.All, .5f, Color.Black),
+                DefaultCellBorder = new BorderInfo(BorderSide.All, .2f, Color.Black)
+
+            };
+            table.ImportDataTable(dataTable, true, 0, 0);
+            document.Pages[1].Paragraphs.Add(table);
+            using (var streamout = new MemoryStream())
+            {
+                document.Save(streamout);
+                return new FileContentResult(streamout.ToArray(), "application/pdf")
+                {
+                    FileDownloadName = "DataPerformance"
+                };
+            }
+        }
+    }
+}
