6652654e7602cdd120c40c2a151d90a085c4ca15
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-09-08 09:52:35 +0200

    OrderValidator1

--- Postieri/Validators/OrderValidator.cs
+++ Postieri/Validators/OrderValidator.cs
@@ -1,6 +1,25 @@
-﻿namespace Postieri.Validators
+﻿using FluentValidation;
+using Postieri.Models;
+
+namespace Postieri.Validators
 {
-    public class OrderValidator
+    public class OrderValidator : AbstractValidator<Order>
     {
+
+        public OrderValidator()
+        {
+            RuleFor(x => x.Address).NotEmpty().NotNull();
+            RuleFor(x => x.OrderId).NotEmpty().NotNull();
+            RuleFor(x => x.Price).NotEmpty().NotNull();
+            RuleFor(x => x.CompanyId).NotEmpty().NotNull();
+            RuleFor(x => x.UserId).NotEmpty().NotNull();
+            RuleFor(x => x.CourierId).NotEmpty().NotNull();
+            RuleFor(x => x.Status).NotEmpty().NotNull();
+            RuleFor(x => x.Date).NotNull().NotEmpty();
+            RuleFor(x => x.ProductId).NotEmpty().NotNull();
+            RuleFor(x => x.ManagerId).NotEmpty().NotNull();
+            RuleFor(x => x.Sign).NotEmpty().NotNull();
+            RuleFor(x => x.OrderedOn).NotEmpty().NotNull();
+        }
     }
 }
