01f99ad3487df6b8ac8d13bca0dd4ef4cf0e1a03
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-09-11 14:37:35 +0200

    Migration for newName:IsSuspended/table:Users

---
+++ Postieri/Migrations/20220908175233_suspend.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Postieri.Migrations
+{
+    public partial class suspend : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsSuspened",
+                table: "Users",
+                newName: "IsSuspended");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsSuspended",
+                table: "Users",
+                newName: "IsSuspened");
+        }
+    }
+}
