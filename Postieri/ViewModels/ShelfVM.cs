96f2b1b3e9df2f582f6087d9a6319f7b4208c09b
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-09-12 12:52:47 +0200

    Added ViewModels

---
+++ Postieri/ViewModels/ShelfVM.cs
@@ -0,0 +1,10 @@
+﻿namespace Postieri.ViewModels
+{
+    public class ShelfVM
+    {
+        public int ShelfId { get; set; }
+        public int WarehouseId { get; set; }
+        public char BinLetter { get; set; }
+        public int MaxProducts { get; set; }
+    }
+}
---
+++ Postieri/ViewModels/WarehouseVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Postieri.ViewModels
+{
+    public class WarehouseVM
+    {
+        public int WarehouseId { get; set; }
+        public string? Name { get; set; }
+
+        public string? Location { get; set; }
+        public double Area { get; set; }
+        public int NumOfShelves { get; set; }
+    }
+}
