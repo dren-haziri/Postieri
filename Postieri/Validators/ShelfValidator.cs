4a1160411946b7635b5a77ac831b1b6ae67fa4f9
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-09-13 15:36:51 +0200

    Removed rule for warehouseId

--- Postieri/Validators/ShelfValidator.cs
+++ Postieri/Validators/ShelfValidator.cs
@@ -5,15 +5,11 @@ namespace Postieri.Validators
 {
     public class ShelfValidator : AbstractValidator<Shelf>
     {
-        public ShelfValidator(Data.DataContext database)
+        public ShelfValidator()
         {
             RuleFor(x => x.MaxProducts).NotEmpty().NotNull().GreaterThanOrEqualTo(0).WithMessage(" {PropertyName} shoud be a positive number!");
             RuleFor(x => x.BinLetter.ToString() ).NotEmpty().NotNull().Length(1).Matches("[A-Z]").WithMessage(" {PropertyName} should contains only one capital letter!");
             RuleFor(x => x.WarehouseId).NotEmpty().NotNull();
-
-            RuleFor(x => x.WarehouseId)
-                .Must(warehouse => database.Warehouse.Any(w => w.WarehouseId == warehouse))
-                .WithMessage("Warehouse with this id does not exist!");
            
         }
     }
