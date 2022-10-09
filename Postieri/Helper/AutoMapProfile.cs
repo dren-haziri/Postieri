6acac310cfb377aa274958be37ca2522b6637804
Ibadetegashi <77028232+Ibadetegashi@users.noreply.github.com>
2022-10-09 18:17:24 +0200

    AutoMapper and Dtos

---
+++ Postieri/Helper/AutoMapProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Postieri.Models;
+
+namespace Postieri.Helper
+{
+    public class AutoMapProfile : Profile
+    {
+        public AutoMapProfile()
+        {
+            //CreateMap<Shelf, WarehouseDto>();
+
+            //CreateMap<Shelf, ShelfWarehouseDto>().ForMember(d => d.Warehouse.Name,
+            //      opt => opt.MapFrom(s => s));
+
+            // CreateMap<Warehouse, WarehouseDto>().ForMember(d => d.ShelvesId, o => o.MapFrom(s => s.Shelves.Select(m => m.ShelfId))); 
+
+            CreateMap<Shelf, ShelfDto>().ReverseMap();
+            CreateMap<Warehouse, WarehouseDto>().ReverseMap();
+            CreateMap<Shelf, ShelfWarehouseDto>();
+            CreateMap<Warehouse, WarehouseShelvesDto>();
+          
+            
+
+
+        }
+    }
+
+}
